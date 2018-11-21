c74e230214ae8a56f995d3a8e0de4547192d2249
Tappasarn Aramboonpong <tappasarn.aramboonpong@agoda.com>
2018-11-21 10:17:46 +0700

    ban-nexttick : create new rule to ban custom call expression

---
+++ src/rules/disallowedInTestsRule.ts
@@ -0,0 +1,38 @@
+import * as ts from "typescript";
+import * as Lint from "tslint";
+
+interface ICustomOption {
+    name: string;
+    message: string;
+}
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public static DEFAULT_FAILURE_STRING = "This method invocation is not allowed to be used on test files";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new DisallowedInTestsWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class DisallowedInTestsWalker extends Lint.RuleWalker {
+    public visitCallExpression(node: ts.CallExpression) {
+        const fileName: string = node && node.getSourceFile().fileName || "";
+        const isTestFile: boolean = new RegExp("\\b" + ".test" + "\\b").test(fileName);
+        const functionName: string = node && node.expression && node.expression.getText() || "";
+        const verifiedCustomOptions: ICustomOption[] = this.verifyAndGetCustomOption(functionName);
+
+        if (isTestFile && verifiedCustomOptions.length > 0) {
+            this.addFailure(
+                this.createFailure(node.getStart(),
+                node.getWidth(),
+                verifiedCustomOptions[0].message || Rule.DEFAULT_FAILURE_STRING));
+        }
+        super.visitCallExpression(node);
+    }
+
+    private verifyAndGetCustomOption(functionName: string): ICustomOption[] {
+        const customOptionSetInTslintJson: ICustomOption[] = this.getOptions();
+        const verifiedCustomOptions: ICustomOption[] = customOptionSetInTslintJson.filter(option => option && option.name === functionName);
+        return verifiedCustomOptions;
+    }
+}
